91ac309e1ee7386abe3bc23d15d7d2af00288a8f
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2021-05-02 15:35:45 -0400

    Update trial.py

--- scripts/trial.py
+++ scripts/trial.py
@@ -443,7 +443,7 @@ def main():
     t = Trial(data=args.size, batchNum=args.batch, timeout=100, log=args.log, cc=cc, runNum=args.runNum,
               numToRun=args.numToRun, time=args.time, tcp_rmem=args.rmem,
               tcp_mem=args.mem, tcp_wmem=args.wmem, ports=['5201', '5201'])
-    # t.start()
+    t.start()
     print("All done")
 
 
