b548b640df68ac440c4a541e71c30b8eb06876e1
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2021-10-10 17:19:12 -0400

    Update trialTrimmed.py

--- scripts/trialTrimmed.py
+++ scripts/trialTrimmed.py
@@ -87,7 +87,7 @@ class Trial:
         filePrefix = f'Trial_{self.batchNum}'
         os.system(f'mkdir {filePrefix}')
         os.system(f'{sshPrefix} \"mkdir {filePrefix}\"')
-        os.system(f'{sshPrefix} \"./setup_routes.sh\"') # TODO: make changing between vorma and satellite easier
+        os.system(f'{sshPrefix} \"./setup_routes.sh vorma\"') # TODO: make changing between vorma and satellite easier
         #os.system(f'ssh {self.user}@vorma.cs.wpi.edu \"sudo ./satellite.sh\"')
         for host in self.hosts:
             os.system(f'ssh {self.user}@{host} \"mkdir {filePrefix}\"')
